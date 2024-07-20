ce1debf42d16266f8950edba5bfa5e974cf281a8
Katrin <kateyka1410@gmail.com>
2024-07-20 13:13:48 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Profile from './components/Profile/Profile';
 import userData from "./userData.json";
+import friend from './friend.json';
+import FriendList from './components/FriendList/FriendList';
 
 const App = () => {
   return (
@@ -12,6 +14,7 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friend} />
     </>
   );
 };
